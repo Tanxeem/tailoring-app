175aea03cfe9191ed90e4b433f6f67ebfb92a75d
tanxeem <itx.tanxeem@gmail.com>
2025-05-08 15:55:41 +0300

    db connection and old setting

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
 
-import { DEV_DB_URL, NODE_ENV, PROD_DB_URL } from "./serverConfig.js";
+import { DEV_DB_URL } from "./serverConfig.js";
 
 const connectDB = async() => {
     try {
-        if (NODE_ENV === 'development') {
-                await mongoose.connect(DEV_DB_URL)
-        }else if(NODE_ENV === 'production') {
-            await mongoose.connect(PROD_DB_URL)
-        }
+        await mongoose.connect(DEV_DB_URL)
         console.log(
             '\x1b[42m\x1b[37m\x1b[1m%s\x1b[0m', 
-            `Connected to MongoDB database from ${process.env.NODE_ENV} environment`
+            'Connected to MongoDB database'
           );
     } catch (error) {
         console.log(

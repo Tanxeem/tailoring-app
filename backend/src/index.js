e80d7ffcf09a78e298f93327784fdd1624d12072
Itx_Tanxeem <144957141+Tanxeem@users.noreply.github.com>
2025-05-09 02:40:56 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(cookiesparser());
 const allowedOrigins = ALLOW_ORIGINS;
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: ['http://localhost:5173' , allowedOrigins],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));

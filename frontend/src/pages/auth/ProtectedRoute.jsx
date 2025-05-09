c62f939035c15c016cfab84ba87681c1b4db8f64
Itx_Tanxeem <144957141+Tanxeem@users.noreply.github.com>
2025-05-09 03:12:40 +0300

    Update ProtectedRoute.jsx

--- frontend/src/pages/auth/ProtectedRoute.jsx
+++ frontend/src/pages/auth/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Outlet, useNavigate } from "react-router"
 import axios from "axios"   
+import { backendUrl } from "../../App";
 
 function ProtectedRoute() {
     const [isAuth, setIsAuth] = useState(null)
@@ -8,7 +9,7 @@ function ProtectedRoute() {
     const navigate = useNavigate();
 
     useEffect( ()=> {
-        axios.get('http://localhost:3000/api/v1/user/me', {withCredentials: true})
+        axios.get(`${backendUrl}/api/v1/user/me`, {withCredentials: true})
         .then( () => setIsAuth(true))
         .catch( () => setIsAuth(false))
     },[])

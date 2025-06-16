fcbc7352d5627f95c550b3ae5a6431a59212460e
Itx_Tanxeem <144957141+Tanxeem@users.noreply.github.com>
2025-06-17 01:36:41 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://172.105.83.167:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
-      },
-    },
-  },
 })

298cef3f64b342ce978314685944d3a92ae35ed8
Itx_Tanxeem <144957141+Tanxeem@users.noreply.github.com>
2025-06-17 19:19:53 +0300

    Update serverConfig.js

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -10,4 +10,6 @@ export const PROD_DB_URL = process.env.PROD_DB_URL;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
 
-export const ALLOW_ORIGINS = process.env.ALLOW_ORIGINS;
+export const ALLOW_ORIGINS = process.env.ALLOW_ORIGINS 
+  ? process.env.ALLOW_ORIGINS.split(',') 
+  : [];
